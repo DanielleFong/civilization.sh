b686e93ccac88a8fc82396878867fe0bea48a5e2
Danielle Fong <dani.fong@gmail.com>
2026-09-02 05:48:33 -0700

    OBS WGC stream with audio, quality selector, DVR scrubber

--- video/stream.cmd
+++ video/stream.cmd
@@ -1,6 +1,6 @@
 @echo off
+rem transcode OBS 4K HLS (video+audio) to a 1080p rendition for phones; master playlist is static (live.m3u8)
 :loop
-set SID=%RANDOM%%RANDOM%
-"C:\ProgramData\chocolatey\bin\ffmpeg.exe" -hide_banner -loglevel warning -probesize 50M -f gdigrab -framerate 30 -draw_mouse 0 -i title="Sid Meier's Civilization VI (DX12)" -filter_complex "[0:v]format=yuv420p,split=2[a][b];[a]scale=3840:-2[v4k];[b]scale=1920:-2[v1080]" -map "[v4k]" -c:v:0 h264_nvenc -preset p4 -tune ll -rc cbr -b:v:0 14M -maxrate:v:0 14M -bufsize:v:0 28M -g 60 -bf 0 -map "[v1080]" -c:v:1 h264_nvenc -preset p4 -tune ll -rc cbr -b:v:1 4M -maxrate:v:1 4M -bufsize:v:1 8M -g 60 -bf 0 -f hls -hls_time 2 -hls_list_size 8 -hls_flags delete_segments+independent_segments+temp_file -var_stream_map "v:0,name:4k v:1,name:1080" -master_pl_name live.m3u8 -hls_segment_filename "C:\Users\danie\cc\civilization.sh\video\frames\hls\%%v_%SID%_s%%05d.ts" "C:\Users\danie\cc\civilization.sh\video\frames\hls\%%v.m3u8"
+"C:\ProgramData\chocolatey\bin\ffmpeg.exe" -hide_banner -loglevel warning -re -i "C:\Users\danie\cc\civilization.sh\video\frames\hls\obs.m3u8" -vf "scale=1920:-2" -c:v h264_nvenc -preset p4 -tune ll -rc cbr -b:v 4M -maxrate 4M -bufsize 8M -g 60 -bf 0 -c:a copy -f hls -hls_time 2 -hls_list_size 1800 -hls_flags delete_segments+independent_segments+temp_file -hls_segment_filename "C:\Users\danie\cc\civilization.sh\video\frames\hls\1080_%RANDOM%_s%%05d.ts" "C:\Users\danie\cc\civilization.sh\video\frames\hls\1080.m3u8"
 timeout /t 3 >nul
 goto loop
